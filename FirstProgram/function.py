3251441224b43c7e8b754874c752fe331de81fd3
Shock Liang <shockwater@gmail.com>
2018-01-25 22:49:41 +0800

    Built-in function exercises

---
+++ FirstProgram/function.py
@@ -0,0 +1,17 @@
+# Convert int
+print(int("5"))
+
+# Convert float
+print(float("5.6"))
+
+# Convert boolean
+print(bool("True"))
+
+# Calculate length
+print(len("Hello"))
+print(len(["a", "b", "c"]))
+
+# Sort
+print(sorted([16, 24, 99, 83, 17, 2, 9, 57, 111]))
+print(sorted(["B", "b", "A", "a", "D", "G"]))
+print(sorted(["A", "0.883", "7", "a", "Z", "4"]))
