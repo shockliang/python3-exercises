87a6a1c85086cd4b9d7ba0eeaf33f2fbe17c047e
Shock Liang <shockwater@gmail.com>
2018-01-26 22:39:39 +0800

    Condition statement.

---
+++ FirstProgram/condition.py
@@ -0,0 +1,12 @@
+
+
+check = "Hamburger"
+
+if check == False:
+    print("It is false")
+elif check == "Hamburger":
+    print("Yummm")
+elif check == "Yo":
+    print("Hello")
+else:
+    print("It is true")
