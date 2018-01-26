8fa5bd761d6434ed926254e35968b418122c979f
Shock Liang <shockwater@gmail.com>
2018-01-26 22:14:50 +0800

    Keyword arguments.

--- FirstProgram/function.py
+++ FirstProgram/function.py
@@ -4,10 +4,14 @@ def my_function(str1, str2):
     print(str1)
     print(str2)
 
+
 my_function("This is arg 1", "This is second arg")
 
 
-def print_something(name = "Someone", age = "Unknow"):
+def print_something(name="Someone", age="Unknow"):
     print("My name is", name, "and my age is", age)
 
-print_something("Shock")
+
+print_something(None, 27)
+
+print_something(age = 35, name = "Shock")
