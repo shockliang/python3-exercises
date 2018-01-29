58a3e956b904720900b65d5dd41870eef93c206c
Shock Liang <shockwater@gmail.com>
2018-01-29 22:47:06 +0800

    Enemy instantiation.

--- RPGBattle/classes/game.py
+++ RPGBattle/classes/game.py
@@ -61,14 +61,14 @@ class Person:
 
     def choose_action(self):
         i = 1
-        print("Actions")
+        print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
         for item in self.actions:
             print(str(i) + ":", item)
             i += 1
 
     def choose_magic(self):
         i = 1
-        print("Magic")
+        print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
         for spell in self.magic:
             print(str(i) + ":", spell["name"],
                   "(cost:", str(spell["cost"]) + ")")
--- RPGBattle/main.py
+++ RPGBattle/main.py
@@ -6,7 +6,24 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
-print(player.generate_spell_magic(0))
-print(player.generate_spell_magic(1))
-print(player.generate_spell_magic(2))
+running = True
+
+print(bcolors.FAIL + bcolors.BOLD + "AN EMENY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("========================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP:",enemy.get_hp())
+    
+    enemy_choice = 1
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
