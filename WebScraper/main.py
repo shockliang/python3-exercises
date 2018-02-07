c42095cc7e2552d2425ea6996d469a9a6fe7f053
Shock Liang <shockwater@gmail.com>
2018-02-07 20:50:33 +0800

    Parsing soup.

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -6,4 +6,13 @@ params = {"q": search}
 r = requests.get("http://www.bing.com/search", params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
+
