c832721487657bfdaf8b9da4e51ea14ced61d7dd
Shock Liang <shockwater@gmail.com>
2018-02-09 23:55:00 +0800

    Bulk inserts.

--- PyMongo/main.py
+++ PyMongo/main.py
@@ -7,5 +7,10 @@ db = client.mydb
 users = db.users
 user1 = {"username": "Shock", "password": "mypassword",
          "favorite_number": 5, "hobbies": ["program", "gunpla", "games"]}
-user_id = users.insert_one(user1).inserted_id
-print(user_id)
+# user_id = users.insert_one(user1).inserted_id
+user_collection = [{"username":"Rich", "password":"12345"},
+                   {"username":"Nick", "password":"45678"},
+                   {"username":"Mama", "password":"91234"}]
+
+inserted = users.insert_many(user_collection)
+print(inserted.inserted_ids)
