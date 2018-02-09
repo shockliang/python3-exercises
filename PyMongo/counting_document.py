dbf39dba85889bb3f719699c1e49d499aa964b63
Shock Liang <shockwater@gmail.com>
2018-02-10 00:01:04 +0800

    Counting documents.

---
+++ PyMongo/counting_document.py
@@ -0,0 +1,9 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.mydb
+users = db.users
+print("Total items:",users.find().count())
+
+print("Favorite number equal to 5. count:",users.find({"favorite_number": 5}).count())
