a7f303a92904915c900848d137c888d9da40decd
Shock Liang <shockwater@gmail.com>
2018-02-09 23:47:09 +0800

    Inserting documents.

--- PyMongo/main.py
+++ PyMongo/main.py
@@ -3,4 +3,9 @@ from pymongo import MongoClient
 
 # client = MongoClient("localhost", 27017)
 client = MongoClient('mongodb://localhost:27017/')
-db = client.pytests
+db = client.mydb
+users = db.users
+user1 = {"username": "Shock", "password": "mypassword",
+         "favorite_number": 5, "hobbies": ["program", "gunpla", "games"]}
+user_id = users.insert_one(user1).inserted_id
+print(user_id)
