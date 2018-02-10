befcb7cce91558a2904e57da52930d5bacf3e48b
Shock Liang <shockwater@gmail.com>
2018-02-10 15:17:37 +0800

    Multiple find conditins.

--- PyMongo/counting_document.py
+++ PyMongo/counting_document.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client.mydb
 users = db.users
-print("Total items:",users.find().count())
+print("Total items:", users.find().count())
 
-print("Favorite number equal to 5. count:",users.find({"favorite_number": 5}).count())
+print("Favorite number equal to 5. count:", users.find(
+    {"favorite_number": 5, "username": "Shock"}).count())
